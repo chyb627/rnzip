c49ffafcf73f4db3ceaa529b1c9f09efbf2d63e4
cha <chyb627@naver.com>
2023-01-22 22:00:29 +0900

    Calculator

---
+++ src/hooks/Calculator/use-calculator.ts
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+
+export const useCalculator = () => {
+  const [input, setInput] = useState<number>(0);
+  const [currentOperator, setCurrentOperator] = useState<'+' | '-' | '*' | '/' | '=' | null>(null);
+  const [result, setResult] = useState<number>(0);
+  const [tempInput, setTempInput] = useState<number>(0);
+  const [tempOperator, setTempOperator] = useState<string | null>(null);
+  const [isClickedOperator, setIsClickedOperator] = useState<boolean>(false);
+  const [isClickedEqual, setIsClickedEqual] = useState<boolean>(false);
+
+  const hasInput = !!input; // input ? true : false;
+
+  const onPressNum = (num: number) => {
+    if (currentOperator && isClickedOperator) {
+      setResult(input);
+      setInput(num);
+      setIsClickedOperator(false);
+    } else {
+      const newInput = Number(`${input}${num}`);
+      setInput(newInput);
+    }
+  };
+
+  const onPressOperator = (operator: '+' | '-' | '*' | '/' | '=') => {
+    if (operator !== '=') {
+      setCurrentOperator(operator);
+      setIsClickedOperator(true);
+      setIsClickedEqual(false);
+    } else {
+      let finalResult = result;
+      const finalInput = isClickedEqual ? tempInput : input;
+      const finalOperator = isClickedEqual ? tempOperator : currentOperator;
+
+      switch (finalOperator) {
+        case '+':
+          finalResult = result + finalInput;
+          break;
+        case '-':
+          finalResult = result - finalInput;
+          break;
+        case '*':
+          finalResult = result * finalInput;
+          break;
+        case '/':
+          finalResult = result / finalInput;
+          break;
+        default:
+          break;
+      }
+      setResult(finalResult);
+      setInput(finalResult);
+      setTempInput(finalInput);
+      setCurrentOperator(null);
+      setTempOperator(finalOperator);
+      setIsClickedEqual(true);
+    }
+  };
+
+  const onPressReset = () => {
+    if (hasInput) {
+      setInput(0);
+    } else {
+      setInput(0);
+      setCurrentOperator(null);
+      setResult(0);
+      setTempInput(0);
+      setTempOperator(null);
+    }
+  };
+
+  return {
+    input,
+    currentOperator,
+    hasInput,
+    onPressNum,
+    onPressOperator,
+    onPressReset,
+  };
+};
--- src/screens/Calculator.tsx
+++ src/screens/Calculator.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useState } from 'react';
+import React from 'react';
 import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import styled from 'styled-components/native';
+import { useCalculator } from '../hooks/Calculator/use-calculator';
 
 const COLOR = {
   RESULT: '#4e4c51',
@@ -10,12 +11,13 @@ const COLOR = {
   NUM: '#5c5674',
 };
 
-const Button: React.FC<{ text: string; onPress: () => void; flex: number; type: string }> = ({
-  text,
-  onPress,
-  flex,
-  type,
-}) => {
+const Button: React.FC<{
+  text: string;
+  onPress: () => void;
+  flex: number;
+  type: string;
+  isSelected?: boolean;
+}> = ({ text, onPress, flex, type, isSelected }) => {
   const backgroundColor =
     type === 'reset'
       ? COLOR.RESET
@@ -34,7 +36,7 @@ const Button: React.FC<{ text: string; onPress: () => void; flex: number; type: 
         justifyContent: 'center',
         alignItems: 'center',
         height: 50,
-        borderWidth: 0.2,
+        borderWidth: isSelected ? 1 : 0.2,
         borderColor: '#000',
       }}>
       <Text style={{ color: 'white', fontSize: 25 }}>{text}</Text>
@@ -51,14 +53,12 @@ const InputContainer = styled.View`
   min-height: 50px;
   justify-content: center;
   align-items: flex-end;
+  padding: 10px 5px; // (top, bottom)-vertical (right, left)-holizontal
 `;
 
 export const Calculator = () => {
-  const [input, setInput] = useState(0);
-  const [currentOperator, setCurrentOperator] = useState(null);
-  const [result, setResult] = useState(null);
-  const [tempInput, setTempInput] = useState(null);
-  const [tempOperator, setTempOperator] = useState(null);
+  const { input, currentOperator, hasInput, onPressNum, onPressOperator, onPressReset } =
+    useCalculator();
 
   return (
     <SafeAreaView
@@ -66,43 +66,86 @@ export const Calculator = () => {
       <View style={{ flex: 1, width: 250, justifyContent: 'center' }}>
         {/* 결과 */}
         <InputContainer>
-          <Text style={{ color: 'white', fontSize: 35 }}>{input}</Text>
+          <Text style={{ color: 'white', fontSize: 35, textAlign: 'right' }}>{input}</Text>
         </InputContainer>
 
         {/* [AC ~ /] */}
         <ButtonContainer>
-          <Button type="reset" text="AC" onPress={() => null} flex={3} />
-          <Button type="operator" text="/" onPress={() => null} flex={1} />
+          <Button type="reset" text={hasInput ? 'C' : 'AC'} onPress={onPressReset} flex={3} />
+          <Button
+            type="operator"
+            text="/"
+            onPress={() => onPressOperator('/')}
+            flex={1}
+            isSelected={currentOperator === '/'}
+          />
         </ButtonContainer>
 
         {/* [7 ~ x] */}
         <ButtonContainer>
-          <Button type="num" text="7" onPress={() => null} flex={1} />
-          <Button type="num" text="8" onPress={() => null} flex={1} />
-          <Button type="num" text="9" onPress={() => null} flex={1} />
-          <Button type="operator" text="X" onPress={() => null} flex={1} />
+          {[7, 8, 9].map((num) => (
+            <Button
+              type="num"
+              text={`${num}`}
+              onPress={() => onPressNum(num)}
+              flex={1}
+              key={`btn-${num}`}
+            />
+          ))}
+
+          <Button
+            type="operator"
+            text="*"
+            onPress={() => onPressOperator('*')}
+            flex={1}
+            isSelected={currentOperator === '*'}
+          />
         </ButtonContainer>
 
         {/* [4 ~ -] */}
         <ButtonContainer>
-          <Button type="num" text="4" onPress={() => null} flex={1} />
-          <Button type="num" text="5" onPress={() => null} flex={1} />
-          <Button type="num" text="6" onPress={() => null} flex={1} />
-          <Button type="operator" text="-" onPress={() => null} flex={1} />
+          {[4, 5, 6].map((num) => (
+            <Button
+              type="num"
+              text={`${num}`}
+              onPress={() => onPressNum(num)}
+              flex={1}
+              key={`btn-${num}`}
+            />
+          ))}
+          <Button
+            type="operator"
+            text="-"
+            onPress={() => onPressOperator('-')}
+            flex={1}
+            isSelected={currentOperator === '-'}
+          />
         </ButtonContainer>
 
         {/* [1 ~ +] */}
         <ButtonContainer>
-          <Button type="num" text="1" onPress={() => null} flex={1} />
-          <Button type="num" text="2" onPress={() => null} flex={1} />
-          <Button type="num" text="3" onPress={() => null} flex={1} />
-          <Button type="operator" text="+" onPress={() => null} flex={1} />
+          {[1, 2, 3].map((num) => (
+            <Button
+              type="num"
+              text={`${num}`}
+              onPress={() => onPressNum(num)}
+              flex={1}
+              key={`btn-${num}`}
+            />
+          ))}
+          <Button
+            type="operator"
+            text="+"
+            onPress={() => onPressOperator('+')}
+            flex={1}
+            isSelected={currentOperator === '+'}
+          />
         </ButtonContainer>
 
         {/* [0 ~ =] */}
         <ButtonContainer>
-          <Button type="num" text="0" onPress={() => null} flex={3} />
-          <Button type="operator" text="=" onPress={() => null} flex={1} />
+          <Button type="num" text="0" onPress={() => onPressNum(0)} flex={3} />
+          <Button type="operator" text="=" onPress={() => onPressOperator('=')} flex={1} />
         </ButtonContainer>
       </View>
     </SafeAreaView>
