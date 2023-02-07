33a6c74917943a02ac966f4c6c9b8f2a193e9c22
cha <chyb627@naver.com>
2023-02-07 23:30:34 +0900

    react-native-google-mobile-ads

--- src/screens/Instagram.tsx
+++ src/screens/Instagram.tsx
@@ -5,14 +5,23 @@ import { RootApp } from './Instagram/RootApp';
 import { store } from '../store/store';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import 'react-native-gesture-handler';
+import mobileAds, { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
 GoogleSignin.configure();
+
+mobileAds()
+  .initialize()
+  .then((result) => {
+    console.log('result:::', result);
+  });
 
 export const Instagram = () => {
   return (
     <SafeAreaProvider>
       <Provider store={store}>
         <RootApp />
+
+        <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.FULL_BANNER} />
       </Provider>
     </SafeAreaProvider>
   );
